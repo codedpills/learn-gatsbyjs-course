633f5d8a5c734648f5b6e1ba3d13d21c4c862c0d
zak <codedpills@gmail.com>
2021-01-02 12:54:04 +0000

    state in class component

---
+++ react-bootcamp/my-app/src/ClassCounter.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react';
+
+class ClassCounter extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            count: 4,
+            person: "Zak"
+        }
+    }
+
+    handleIncreaseCount = () => {
+        // console.log("count increased!");
+        this.setState({
+            count: this.state.count + 1
+        })
+    }
+    handleDecreaseCount = () => {
+        // console.log("Count decreased!");
+        this.setState({
+            count: this.state.count - 1
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>Counter: {this.state.count} </h2>
+                <hr />
+                <button onClick={this.handleIncreaseCount}>Increase count</button>
+                <button onClick={this.handleDecreaseCount}>Decrease count</button>
+            </div>
+        )
+    }
+}
+
+export default ClassCounter;
